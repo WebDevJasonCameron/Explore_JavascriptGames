b18a9f2cd9a992ba75136865158a915d3821ace8
Jason Cameron <dev.jasoncameron@gmail.com>
2024-05-19 08:33:09 -0500

    need to rewatch to fully get what I just did

--- Lesson_01/script.js
+++ Lesson_01/script.js
@@ -10,21 +10,43 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 
 let frameX = 0;
-let frameY = 0;
-
+let frameY = 1;
 let gameFrame = 0;
 const staggerFrames = 5;
 
-function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CA NVAS_HEIGHT);
-    // ctx.fillRect(100, 50, 100, 100);
-    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-
-    if (gameFrame % staggerFrames === 0){
-        if (frameX < 6) frameX++;
-        else frameX = 0;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7
+    },
+    {
+        name: 'jump',
+        frames: 7
+    },
+]
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
     }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+})
+
+console.log(spriteAnimations);
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['idle'].loc.length;
+    frameX = spriteWidth * position;
+
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
 
     gameFrame++;
 
