8a1836921b869ebac4515faed31c81b653f4887c
Jason Cameron <dev.jasoncameron@gmail.com>
2024-05-20 06:17:44 -0500

    We have paralax

--- Lesson_02/script.js
+++ Lesson_02/script.js
@@ -17,17 +17,48 @@ backgroundLayer4.src = "assets/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "assets/layer-5.png";
 
-let x = 0;
-let x2 = 2400;                      //  See, this 2nd image is set at the back!
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+
+    update(){
+        this.speed = gameSpeed = this.speedModifier;
+        if (this.x <= -this.width) {
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if (this.x2 <= -this.width) {
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.5);
+const layer2 = new Layer(backgroundLayer2, 0.5);
+const layer3 = new Layer(backgroundLayer3, 0.5);
+const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer5 = new Layer(backgroundLayer5, 2);
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < - 2400) x = 2400
-    x-= gameSpeed;
-    if (x2 < - 2400) x2 = 2400
-    x2-= gameSpeed;
+    layer4.update()
+    layer4.draw();
+    layer5.update()
+    layer5.draw();
+
     requestAnimationFrame(animate);
 }
 
