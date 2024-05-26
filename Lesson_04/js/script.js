bb29c409995e5ccb71734e80ced0800198006491
Jason Cameron <dev.jasoncameron@gmail.com>
2024-05-26 12:50:40 -0500

    added sound with the targetting

--- Lesson_04/js/script.js
+++ Lesson_04/js/script.js
@@ -9,28 +9,83 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.spriteWidth = 200;
+        this.spriteWidth = 200;                                 //   Sprite size
         this.spriteHeight = 179;
-        this.width = this.spriteWidth/2;
-        this.height = this.spriteHeight/2;
 
-        this.image = new Image();
+        this.width = this.spriteWidth * 0.7;                    //   Actual drawn image size
+        this.height = this.spriteHeight * 0.7;
+
+        this.x = x;                                             //   Location on screen
+        this.y = y;
+
+        this.image = new Image();                               //   Image creation
         this.image.src = './assets/boom.png'
-        this.frame = 0;
+
+        this.frame = 0;                                         //   Animation prep
+        this.timer = 0;
+        this.angle = Math.random() * 6.2;
+
+        this.sound = new Audio();                               //   Sound prep
+        this.sound.src = './assets/boom.wav'
     }
 
     update(){
-        this.frame++;
+        if (this.frame === 0) this.sound.play();
+
+        this.timer++;
+        if (this.timer % 10 === 0){
+            this.frame++;
+        }
     }
     draw(){
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.save();
+
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+
+        ctx.drawImage(this.image,
+                    this.spriteWidth * this.frame,
+                    0,
+                    this.spriteWidth,
+                    this.spriteHeight,
+                    0 - this.width / 2,
+                    0 - this.height / 2,
+                    this.width,
+                    this.height);
+
+        ctx.restore();                                  //   restores to the original save point.  w/o this, it locks
     }
 }
 
 window.addEventListener('click', function(e){
-    console.log(e);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(e.x - canvasPosition.left - 25, e.y - canvasPosition.top - 25, 50, 50)     // Centered mouse click
+    createAnimation(e)
 });
+
+/*
+window.addEventListener('mousemove', function(e){       //   Fun animation
+    createAnimation(e)
+})
+*/
+
+function createAnimation(e){
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);           //   Clear each frame
+
+    for (let i = 0; i < explosions.length; i++) {               //   Update then draw
+        explosions[i].update();
+        explosions[i].draw();
+
+        if (explosions[i].frame > 5) {                          //   Removing the explosions from the growing array
+            explosions.splice(i, 1);
+            i--;
+        }
+    }
+    requestAnimationFrame(animate);
+}
+
+animate()
