b41ae4c13f7bda1021a09bb51c16e3e84d96d8a7
Jason Cameron <dev.jasoncameron@gmail.com>
2024-05-31 18:58:41 -0500

    start lsn 7 setup

---
+++ Lesson_07/js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas1');
+    const context = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    // Loading Classes from other js files
+
+
+
+})
