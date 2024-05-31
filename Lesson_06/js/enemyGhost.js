047601d4d0006bf00d7b2e4deecc7954555887fd
Jason Cameron <dev.jasoncameron@gmail.com>
2024-05-31 15:47:16 -0500

    ghost done

--- Lesson_06/js/enemyGhost.js
+++ Lesson_06/js/enemyGhost.js
@@ -12,13 +12,23 @@ class Ghost extends Enemy {
         this.y = Math.random() * this.game.height * 0.6;        // Take only 60% of top space
 
         this.vx = Math.random() * 0.2 + 0.1;                    // vx = vertical x (speed)
+        this.angle = 0;
+        this.curve = Math.random() * 3
 
         this.image = ghost;
+
+    }
+
+    update(deltaTime) {
+        super.update(deltaTime);
+
+        this.y += Math.sin(this.angle) * this.curve;                        // Making waves
+        this.angle += 0.04;
     }
 
     draw(ctx){
         ctx.save()
-        ctx.globalAlpha = 0.5;
+        ctx.globalAlpha = 0.4;
         super.draw(ctx);
         ctx.restore()
     }
--- Lesson_06/js/game.js
+++ Lesson_06/js/game.js
@@ -5,7 +5,7 @@ class Game {
         this.height = height;
 
         this.enemies = [];
-        this.enemyInterval = 100;
+        this.enemyInterval = 500;
         this.enemyTimer = 0;
         this.enemyTypes = ['worm', 'ghost']
     }
