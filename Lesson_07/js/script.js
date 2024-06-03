ca1afc4aa4eb2cdcba9b941f25c2d78724f66311
Jason Cameron <dev.jasoncameron@gmail.com>
2024-06-03 05:27:08 -0500

    fig out why arror keys not showing

--- Lesson_07/js/script.js
+++ Lesson_07/js/script.js
@@ -5,7 +5,24 @@ window.addEventListener('load', () => {
     canvas.height = 720;
 
     // Loading Classes from other js files
+    class InputHandler {
+        constructor() {
+            this.keys = []
+            window.addEventListener('keydown', (e) =>{
+                if (e.key === 's' && this.keys.indexOf(e.key) === -1){
+                    this.keys.push(e.key)
+                }
+                console.log(e.key, this.keys)
+            })
 
+            window.addEventListener('keyup', (e) =>{
+                if (e.key === 's'){
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(e.key, this.keys)
+            })
+        }
+    }
 
     function handleEnemies(){
 
