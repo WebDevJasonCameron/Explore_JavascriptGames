986bbab0ced2e5d724ec739ee62c5e720d66540e
Jason Cameron <dev.jasoncameron@gmail.com>
2024-06-05 05:50:42 -0500

    mvmt keys added

--- Lesson_07/js/script.js
+++ Lesson_07/js/script.js
@@ -8,18 +8,24 @@ window.addEventListener('load', () => {
     class InputHandler {
         constructor() {
             this.keys = []
+
             window.addEventListener('keydown', (e) =>{
-                if (e.key === 's' && this.keys.indexOf(e.key) === -1){
+                if (    (e.key === 's' ||
+                        e.key === 'w' ||
+                        e.key === 'a' ||
+                        e.key === 'd')
+                        && this.keys.indexOf(e.key) === -1){
                     this.keys.push(e.key)
                 }
-                console.log(e.key, this.keys)
             })
 
             window.addEventListener('keyup', (e) =>{
-                if (e.key === 's'){
+                if (    e.key === 's' ||
+                        e.key === 'w' ||
+                        e.key === 'a' ||
+                        e.key === 'd'){
                     this.keys.splice(this.keys.indexOf(e.key), 1);
                 }
-                console.log(e.key, this.keys)
             })
         }
     }
