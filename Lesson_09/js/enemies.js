3f29fc8d05fbd53a3d7008f4646cbb5f8ab7fa84
Jason Cameron <dev.jasoncameron@gmail.com>
2024-07-10 05:47:59 -0500

    add climbing enemy

--- Lesson_09/js/enemies.js
+++ Lesson_09/js/enemies.js
@@ -82,7 +82,31 @@ export class GroundEnemy extends Enemy {
 }
 
 export class ClimbingEnemy extends Enemy {
-    constructor() {
+    constructor(game) {
         super();
+        this.game = game;
+        this.width = 120;
+        this.height = 144;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.image = document.getElementById('enemy-spider')
+        this.speedX = 0;
+        this.speedY = Math.random() > 0.5 ? 1 : -1;
     }
+    update(deltaTime) {
+        super.update(deltaTime);
+        if (this.y > this.game.height - this.height - this.game.groundMargin) this.speedY *= -1;
+        if (this.y < -this.height) this.markedForDeletion = true;
+    }
+
+    draw(context) {
+        super.draw(context);
+        context.beginPath();
+        context.moveTo(this.x + this.width/2, 0)
+        context.lineTo(this.x + this.width/2, this.y + 50);
+        context.stroke();
+    }
+
 }
+
+
